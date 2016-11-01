16ff36617d120488ed1036c7ce916415f01de0d2
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-01 19:36:04 +0100

    SVGs added

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <cassert>
 #include <cctype>
+#include <sstream>
 #include <string>
+#include <fstream>
 
 //From http://www.richelbilderbeek.nl/CppStrToUpper.htm
 std::string to_upper(std::string s) noexcept
@@ -119,7 +121,11 @@ std::string get_codons(const std::string& s)
 
 std::vector<std::string> get_group_name() noexcept
 {
-  return { "Theoreticaly", "Evolutionary", "  Community", " Ecology " };
+  return {
+    "Theoreticaly",
+    "Evolutionary",
+    "  Community ",
+    " Ecology    " };
 }
 
 std::vector<std::string> get_group_name_uppercase() noexcept
@@ -169,6 +175,91 @@ bool is_okay(const std::vector<std::string>& v)
   ;
 }
 
+template <class T>
+std::string get_svg_line(const T x1, const T y1, const T x2, const T y2, const int stroke_width = 1)
+{
+  std::stringstream s;
+  s << "  <line "
+    << "x1=\""<< x1 << "\" "
+    << "y1=\""<< y1 << "\" "
+    << "x2=\""<< x2 << "\" "
+    << "y2=\""<< y2 << "\" "
+    << "style=\"stroke:black;stroke-width:" << stroke_width << "\" />\n"
+  ;
+  return s.str();
+}
+
+std::string create_svg_string()
+{
+  std::stringstream s;
+  s << "<svg height=\"500\" width=\"1500\">\n";
+
+  const int offset_x{200};
+  //Show name
+  {
+    const int offset_y{50};
+    const int width{40};
+    const int height{40};
+    const auto v = get_group_name();
+    for (int y=0; y!=4; ++y)
+    {
+      const int sz = v[y].size();
+      for (int x=0; x!=sz; ++x)
+      {
+        const int x_co = offset_x + (x * width);
+        const int y_co = offset_y + (y * height);
+        s << "  <rect x=\""<< (x_co - 5) << "\" y=\""<< (y_co + 10 - height) << "\" width=\"" << width << "\" height=\"" << height << "\"  style=\"fill:white;stroke:black;stroke-width:2;opacity:1\" />\n";
+        s << "  <text x=\""<< x_co << "\" y=\""<< y_co << "\" font-family=\"Verdana\" font-size=\"30\">" << v[y][x] << "</text>\n";
+      }
+    }
+    //Add phylogeny
+    //s << get_svg_line(offset_x-5,offset_y-10+(0 * height),offset_x-5-50,offset_y-10+(0 * height), 4);
+    //s << get_svg_line(offset_x-5,offset_y-10+(1 * height),offset_x-5-50,offset_y-10+(1 * height), 4);
+    //s << get_svg_line(offset_x-5,offset_y-10+(2 * height),offset_x-5-50,offset_y-10+(2 * height), 4);
+    //s << get_svg_line(offset_x-5,offset_y-10+(3 * height),offset_x-5-50,offset_y-10+(3 * height), 4);
+    /*
+
+    E-A
+  K-F
+  | G-B
+N-L
+  | H-C
+  M-I
+    J-D
+    */
+  }
+  //Show codons
+  {
+    const int offset_y{250};
+    const int width{30};
+    const int height{30};
+    const auto v = get_group_codons();
+    for (int y=0; y!=4; ++y)
+    {
+      const int sz = v[y].size();
+      for (int x=0; x!=sz; ++x)
+      {
+        const int x_co = offset_x + (x * width);
+        const int y_co = offset_y + (y * height);
+        s << "  <rect x=\""<< (x_co - 5) << "\" y=\""<< (y_co + 10 - height) << "\" width=\"" << width << "\" height=\"" << height << "\"  style=\"fill:white;stroke:black;stroke-width:2;opacity:1\" />\n";
+        s << "  <text x=\""<< x_co << "\" y=\""<< y_co << "\" font-family=\"Verdana\" font-size=\"20\">" << v[y][x] << "</text>\n";
+      }
+    }
+  }
+  s << "</svg>\n";
+  return s.str();
+}
+
+void create_svg()
+{
+  {
+    std::ofstream f("tece_logo.svg");
+    f << create_svg_string();
+  }
+  std::system("inkview tece_logo.svg");
+  //std::system("display tece_logo.svg");
+}
+
 int main()
 {
   const auto v = get_group_codons();
@@ -190,8 +281,46 @@ int main()
     << (get_distance(c, d) < get_distance(d, a)) << '\n'
     << (get_distance(c, d) < get_distance(d, b)) << '\n'
   ;
+
+  //Show matrix
+  for (int y = 0; y!=4; ++y)
+  {
+    for (int x = 0; x!=4; ++x)
+    {
+      if (x == y)
+      {
+        std::cout << "-- ";
+      }
+      else
+      {
+        std::cout << get_distance(v[x], v[y]) << " ";
+      }
+    }
+    std::cout << '\n';
+  }
+
   std::cout << "CONCLUSION: " << is_okay(v) << '\n';
+
+  create_svg();
 }
 
+/*
 
+ACACACGAA   CGAGAAACAATTTGCGCATTATAC
+GAAGTA   TTA   ACAATT   AACGCACGATAC
+ATGATGTGC   ATGATG   AACATTACATACATG
+ATGGAATGC   TTA   GGATACATGATGATGATG
+true
+true
+true
+true
+true
+true
+true
+true
+-- 25 28 28
+25 -- 31 33
+28 31 -- 18
+28 33 18 --
 
+*/
